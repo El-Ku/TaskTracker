f6d1604373e00285767f9ae222537d120571f904
El-Ku <elkus.services@gmail.com>
2025-04-19 19:15:35 +0530

    ver 2

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
-const tasks = {};  //contains all the individual task objects
+let tasks = {};  //contains all the individual task objects
 const addTaskBtn = document.getElementById("addBtn");
 const taskTable = document.getElementById("taskTable");
 let latestIndex = 0; //indicates the next property name to which new task willsl be added.
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    if(tasks == undefined) {
+        console.log("No tasks in memory. Create something fresh");
+        tasks = {};
+    }
+    else {  //some tasks found in localstorage. Show them in a table format
+        for (const index in tasks) {
+            updateTable(Number(index)); 
+        }
+    }
+});
 
 // what happens when you click the add Task button is specified here.
 addTaskBtn.addEventListener("click", () => {
     let taskItems = document.getElementById("taskInput").value;
     taskItems = taskItems.split(",");  //get an array of tasks if entered as comma separated
     // Add each task into separate rows. one by one.
+    console.log(taskItems, latestIndex)
     taskItems.forEach((taskItem) => {
+        console.log(tasks[String(latestIndex)])
         tasks[String(++latestIndex)] = {
             taskId: latestIndex,
             taskName: taskItem,
             timeAdded: Date.now(),
             done: false
         };
-        updateTable();  // add a new row to the table with the new task.
+        updateTable(latestIndex);  // add a new row to the table with the new task.
     });
+    localStorage.setItem("tasks", JSON.stringify(tasks));  //save the tasks object in localstorage
     document.getElementById("taskInput").value = "";  //clear the input text box.
 });
 
 // Add one row to the table(which is the latest added task)
-const updateTable = () => {
-    let item = tasks[String(latestIndex)]; //get the latest task from the tasks object
+const updateTable = (index) => {
+    let item = tasks[String(index)]; //get the task pointed by index from the tasks object
     addRow(item); //add a row
 };
 
@@ -64,12 +81,17 @@ const addRow = (item) => {
     cell.appendChild(doneBtn);
     cell.appendChild(cancelBtn);
     cell.appendChild(editBtn);
+    // useful when read from localstorage
+    if(item.done) {
+        row.style.backgroundColor = "green";
+    }
 };
 
 // remove a row from the table and delete the property from the tasks object
 const removeTaskFromArray = (rowElement) => {
     const taskId = rowElement.cells[0].textContent;  //get the task ID.
     delete tasks[taskId];
+    localStorage.setItem("tasks", JSON.stringify(tasks));  // update localstorage
     rowElement.remove();  //remove the row from the table
 };
 
@@ -77,6 +99,7 @@ const removeTaskFromArray = (rowElement) => {
 const setTaskDone = (rowElement) => {
     const taskId = rowElement.cells[0].textContent; //get the task ID.
     tasks[taskId].done = true;  //remove the task item from the tasks object.
+    localStorage.setItem("tasks", JSON.stringify(tasks));   // update localstorage
     rowElement.style.backgroundColor = "green";
 };
 
@@ -100,6 +123,7 @@ const editTask = (rowElement) => {
             taskCell.textContent = input.value;
             const taskId = rowElement.cells[0].textContent; //get the task ID.
             tasks[taskId].taskName = input.value;  //update task name on tasks object
+            localStorage.setItem("tasks", JSON.stringify(tasks));    // update localstorage
         } else if (e.key === "Escape") {
             taskCell.textContent = currentTask;
         }
